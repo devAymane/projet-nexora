86f1735f34a75e90bab70adcaa03fdac1ba0a8a3
aymanexiaomi07@gmail.com <ton@email.com>
2026-09-04 16:38:43 +0100

    feat: add category policy authorization

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,26 +2,23 @@
 
 namespace App\Providers;
 
+use App\Models\Category;
 use App\Models\Service;
+use App\Policies\CategoryPolicy;
 use App\Policies\ServicePolicy;
-use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
         Gate::policy(Service::class, ServicePolicy::class);
+        Gate::policy(Category::class, CategoryPolicy::class);
     }
 }
