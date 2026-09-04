f6e2f9ce640b9a432e7008a86a1a28693217476f
aymanexiaomi07@gmail.com <ton@email.com>
2026-09-04 15:57:00 +0100

    feat: add service routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ServiceController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -90,3 +92,41 @@ Route::middleware(['auth', 'nexora.role:provider'])->group(function () {
 */
 
 require __DIR__.'/auth.php';
+
+
+
+
+
+
+
+
+
+/*
+|--------------------------------------------------------------------------
+| Services Routes
+|--------------------------------------------------------------------------
+*/
+
+Route::middleware('auth')->group(function () {
+
+    Route::get('/services', [ServiceController::class, 'index'])
+        ->name('services.index');
+
+    Route::get('/services/create', [ServiceController::class, 'create'])
+        ->name('services.create');
+
+    Route::post('/services', [ServiceController::class, 'store'])
+        ->name('services.store');
+
+    Route::get('/services/{service}', [ServiceController::class, 'show'])
+        ->name('services.show');
+
+    Route::get('/services/{service}/edit', [ServiceController::class, 'edit'])
+        ->name('services.edit');
+
+    Route::put('/services/{service}', [ServiceController::class, 'update'])
+        ->name('services.update');
+
+    Route::delete('/services/{service}', [ServiceController::class, 'destroy'])
+        ->name('services.destroy');
+});
